272697102083f5b71086be5d0c2cf906dd9e8984
Rafael Contreras <rcontreraspimentel@gmail.com>
2021-02-14 18:19:59 -0400

    ready for github pages

--- src/components/aboutContent.js
+++ src/components/aboutContent.js
@@ -80,11 +80,19 @@ const AboutMe = (props)=>{
 						margin:0.5em 1em 0 1em;
 					`}>
 					<p>
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur.
+					Welcome to my portfolio!
+
+					I´m a Computer Science student living at Caracas, Venezuela. I started programming when I was 17 and since then I´m passionate with it. 
+					</p>
+					<br/>
+					<p>
+					When it comes to building applications I focus mostly on its performance, scalability and user experience. I love to set up animations and improve my code´s speed and eficiency. 
+					Also I know many things and technologies related to backend, nevertheless I´m more proficent building the frontend.
+					</p>
+					<br/>
+					<p>
+					I love to build software and learn new tools, techniques and technologies. One of my favorite ones is React, mostly because of its flexibility.
+					Also I love to use Javascript as my principal programming language.
 					</p>
 				</BorderedStyled>
 			</article>
